02b6298128f3b37420a0c6ca7961f187971e775c
Juho S <juho.salli@gmail.com>
2022-12-01 09:01:48 +0200

    funktioluennon koodi

---
+++ funktiot/funktio-luento/Program.cs
@@ -0,0 +1,12 @@
+﻿int TulostaSanaJaPalautaSenPituus(string sana)
+{
+    Console.WriteLine(sana);
+    int sanan_pituus = sana.Length;
+
+    return sanan_pituus;
+}
+
+int pituus = TulostaSanaJaPalautaSenPituus("moro");
+Console.WriteLine("pituus: " + pituus);
+
+// tulosta_sana_ja_palauta_sen_pituus
