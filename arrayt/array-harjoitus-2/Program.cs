bdba7cc3cb227acab5dbd6c1f06c5a984853fc0e
Juho S <juho.salli@gmail.com>
2022-12-01 09:01:17 +0200

    arrayharkkaratkaisuihin muutoksia

--- arrayt/array-harjoitus-2/Program.cs
+++ arrayt/array-harjoitus-2/Program.cs
@@ -3,3 +3,16 @@
 float summa = luvut[0] + luvut[1] + luvut[2];
 
 Console.WriteLine(summa);
+
+
+double[] luvut2 = { 1.1, 1.2, 1.3 };
+
+double summa2 = luvut2[0] + luvut2[1] + luvut2[2];
+
+Console.WriteLine(summa2);
+
+decimal[] luvut3 = { 1.1m, 1.2m, 1.3m };
+
+decimal summa3 = luvut3[0] + luvut3[1] + luvut3[2];
+
+Console.WriteLine(summa3);
--- arrayt/array-harjoitus-5/Program.cs
+++ arrayt/array-harjoitus-5/Program.cs
@@ -1,19 +1,21 @@
-﻿Console.Write("Luku: ");
-string luku1 = Console.ReadLine();
+﻿// Console.Write("Luku: ");
+// string luku1 = Console.ReadLine();
 
-Console.Write("Luku: ");
-string luku2 = Console.ReadLine();
+// Console.Write("Luku: ");
+// string luku2 = Console.ReadLine();
 
-Console.Write("Luku: ");
-string luku3 = Console.ReadLine();
+// Console.Write("Luku: ");
+// string luku3 = Console.ReadLine();
 
-// muutetaan merkkijonot kokonaisluvuiksi taulukon luonnissa
-int[] luvut = { int.Parse(luku1), int.Parse(luku2), int.Parse(luku3) };
+// // muutetaan merkkijonot kokonaisluvuiksi taulukon luonnissa
+// int[] luvut = { int.Parse(luku1), int.Parse(luku2), int.Parse(luku3) };
 
-// Console.Write("Anna 3 lukua pilkulla eroteltuna: ");
-// string input = Console.ReadLine();
-// string[] luvut_str = input.Split(",");
-// int[] luvut = { int.Parse(luvut_str[0]), int.Parse(luvut_str[1]), int.Parse(luvut_str[2]) };
+Console.Write("Anna 3 lukua pilkulla eroteltuna: ");
+string input = Console.ReadLine();
+
+string[] luvut_str = input.Split(",");
+
+int[] luvut = { int.Parse(luvut_str[0]), int.Parse(luvut_str[1]), int.Parse(luvut_str[2]) };
 
 Console.Write("Suoritetaanko luvuille kertolasku (*), summaus (+) vai vähennys (-)?: ");
 string operaatio = Console.ReadLine();
